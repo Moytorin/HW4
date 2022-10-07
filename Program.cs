88d200d85bc937d1656be5ad6a9bda4f7dd28d1c
Mikhail <113309692+Moytorin@users.noreply.github.com>
2022-10-07 20:33:28 +0300

    Add files via upload
    002

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Введите число A: ");
-int A = Convert.ToInt32(Console.ReadLine()!);
-Console.WriteLine("Введите число B: ");
-int B = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-int GetDegree (int A, int B)
+int GetNum(int number)
 {
-    int result = 1;
-    for (int i = 1; i <= B; i++)
+    int result = 0;
+    int a = 0;
+    int b = Convert.ToString(number).Length;
+
+    for (int i = 0; i < b; i++)
     {
-        result = result * A; // а можно записать вот так ((result *= A)) -> будет то же самое
+        a = number - number % 10;
+        result = result + (number - a);
+        number = number/10;
     }
     return result;
 }
-int Stepen = GetDegree(A,B); //эта строка считывает метод
-Console.Write(Stepen);
+int summa = GetNum(number);
+Console.WriteLine("сумма цифр: " + summa);
