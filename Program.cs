59eb50d2998ab6a2f0d8cce80da5ef9a1b387af5
Mikhail <113309692+Moytorin@users.noreply.github.com>
2022-10-07 20:34:16 +0300

    Add files via upload
    003

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
-﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Enter a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// viod - eto metod kotoriu nichego ne vozvraschaet
 
-int GetNum(int number)
-{
-    int result = 0;
-    int a = 0;
-    int b = Convert.ToString(number).Length;
-
-    for (int i = 0; i < b; i++)
+void FillArray(int[] collection) // collection - это название аргумента
+ {
+    int length = collection.Length; // здесь мы получаем длину массива
+    int index = 0; // просто взяли позицию index и по умолчанию присвоили ей 0 
+    while(index < length) // что-то делаем
     {
-        a = number - number % 10;
-        result = result + (number - a);
-        number = number/10;
+        collection[index] = new Random().Next(1,100);   //обращаемся к агрументу коллекшн на позицию индекс и ложем туда новое случайное число 
+        index++; // просто увеличиваем индех на 1
     }
-    return result;
+ }
+
+void PrintArray(int[] col) // metod void kotorui pe4ataet massiv, v ka4estve argumenta zdecb bydet prixodit' massiv "col"
+{
+    int count = col.Length; // obozna4ili kol-vo elementov
+    int posotion = 0; // obozna4ili tekyschyu pozociu 
+    while(posotion<count)
+    {
+        Console.WriteLine(col[posotion]); // vivodim pozociu tekyschego elementa
+        posotion++; // yvelichivaem poziciu tekyschego elementa
+    }
 }
-int summa = GetNum(number);
-Console.WriteLine("сумма цифр: " + summa);
+
+int[] array = new int[8]; // создай новый массив в котором будет 8 элементов, по умолч. он наполнен нулями [0]
+
+FillArray(array); // vizivaev massiv
+PrintArray(array); // vivodim massiv
